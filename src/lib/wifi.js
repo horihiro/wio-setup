b84ac04b363a2a0f0d08cf314d82c0505b02b278
Hirofumi Horikawa <horihiro@nts.ricoh.co.jp>
2017-05-02 14:23:00 +0900

    implement for darwin

--- src/lib/wifi.js
+++ src/lib/wifi.js
@@ -1,7 +1,24 @@
+import {exec} from 'child_process';
+
 export default class Wifi {
-  static scan() {
-    return new Promise((resolve) => {
-      resolve(['WIO_HOGE', 'WIO_FUGA', 'OHHO_OIO']);
+  static getCurrentSsid() {
+    return new Promise((resolve, reject) => {
+      switch(process.platform) {
+        case 'darwin':
+          exec('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I', (err, stdout, stderr) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            const ssid = stdout.split(/\n/).filter(l => /^[ ]*SSID/.test(l)).map((l => l.replace(/^[ ]*SSID: /, '')))[0];
+            resolve(ssid);
+          });
+          break;
+        case 'win32':
+          break;
+        case 'linux':
+          break;
+      }
     });
   }
 }
